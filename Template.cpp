8e40e263fa259c67eaea99f34894a59f6ebfec5a
arthurxsz <arthur.bispo4@gmail.com>
2024-01-18 23:09:18 -0300

    Add Template

---
+++ Template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const int LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+int main(){
+
+}
