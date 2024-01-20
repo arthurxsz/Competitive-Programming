b3ac283999c693a87141b6860c5ad03f5700d5b7
arthurxsz <arthur.bispo4@gmail.com>
2024-01-20 13:28:31 -0300

    Update Template

--- Template.cpp
+++ Template.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define ll long long
 #define all(x) x.begin(), x.end()
 #define pii pair<int, int>
@@ -8,6 +9,6 @@ using namespace std;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-int main(){
+int main(){ _
 
 }
