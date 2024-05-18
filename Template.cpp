7ea6efa98f1b25f89b26c9400f70be53b263cf15
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2024-05-18 11:59:51 -0300

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
-#define ll long long
+#define int long long
 #define all(x) x.begin(), x.end()
 #define pii pair<int, int>
 
 using namespace std;
 
-int main(){ _
+signed main(){ _
 
 }
