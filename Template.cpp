83f333909edc31ab5a771fe6259b9bf619b26adb
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2025-01-26 09:39:26 -0300

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -6,7 +6,7 @@
 #endif
 
 #include <bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
 #define int long long
 #define all(x) x.begin(), x.end()
@@ -15,6 +15,6 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-signed main(){ _
+signed main(){ fastio
     
 }
